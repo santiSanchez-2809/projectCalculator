47b253ec2fb19a3ec266ebfdd4c20067b4c7988f
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-07 13:57:21 -0300

    Add updateVariable and eventListener to number buttons. Fix operator functions so they work with strings

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function add(a, b){
-    return a + b;
+
+    return +a + +b;
 }
 
 function substract(a, b){
-    return a - b;
+    return +a - +b;
 }
 
 function multiply(a, b){
-    return a * b; 
+    return +a * +b; 
 }
 
 function divide(a, b){
-    return a / b;
+    return +a / +b;
 }
 
 let number1, number2, operator;
@@ -32,3 +33,23 @@ function operate(num1, num2, op){
 
     return 0; 
 }
+
+function updateVariable(num){
+    let display = document.querySelector(".display");
+
+    if(operator){
+        number2 = number2? number2 + num: num;
+        display.textContent = number2;
+    }else{
+        number1 = number1? number1 + num: num;
+        display.textContent = number1;
+    }
+     
+    
+}
+
+
+let numberButtons = document.querySelectorAll(".numbers > *");
+numberButtons.forEach((button => button.addEventListener("click", (e) => { 
+    updateVariable(e.target.textContent); 
+})))
